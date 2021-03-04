a0aabd9b93a9bcbed80bd1f669fd02e09bacc8d7
kclaudeeager <71407602+kclaudeeager@users.noreply.github.com>
2021-03-04 13:48:41 +0200

    Billing management system  in java swing

---
+++ Billing_Management_System/src/ConnectionProvider/Connections.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConnectionProvider;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Kwizera
+ */
+public class Connections {
+    public static Connection getcon(){
+        
+    try{
+       Class.forName("com.mysql.jdbc.Driver");
+
+        Connection conn=DriverManager.getConnection( "jdbc:mysql://localhost:3306/bms","root","");
+        return conn;
+    }   catch (Exception ex) {
+           return null;
+        }
+    
+    }
+    
+}
